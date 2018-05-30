3e94fab4279a261df8f401003ae66ecd416ee81d
ceemion <fiyinsimeon@gmail.com>
2018-05-30 18:25:07 +0100

    Refactor drinks and ingredients controller, make slimmer

--- spec/routing/drinks_routing_spec.rb
+++ spec/routing/drinks_routing_spec.rb
@@ -4,29 +4,27 @@ RSpec.describe DrinksController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/drinks").to route_to("drinks#index")
+      expect(:get => "/api/drinks").to route_to("drinks#index")
     end
-
 
     it "routes to #show" do
-      expect(:get => "/drinks/1").to route_to("drinks#show", :id => "1")
+      expect(:get => "/api/drinks/1").to route_to("drinks#show", :id => "1")
     end
 
-
     it "routes to #create" do
-      expect(:post => "/drinks").to route_to("drinks#create")
+      expect(:post => "/api/drinks").to route_to("drinks#create")
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/drinks/1").to route_to("drinks#update", :id => "1")
+      expect(:put => "/api/drinks/1").to route_to("drinks#update", :id => "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/drinks/1").to route_to("drinks#update", :id => "1")
+      expect(:patch => "/api/drinks/1").to route_to("drinks#update", :id => "1")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/drinks/1").to route_to("drinks#destroy", :id => "1")
+      expect(:delete => "/api/drinks/1").to route_to("drinks#destroy", :id => "1")
     end
 
   end
--- spec/routing/ingredients_routing_spec.rb
+++ spec/routing/ingredients_routing_spec.rb
@@ -4,29 +4,27 @@ RSpec.describe IngredientsController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/ingredients").to route_to("ingredients#index")
+      expect(:get => "/api/ingredients").to_not route_to("ingredients#index")
     end
-
 
     it "routes to #show" do
-      expect(:get => "/ingredients/1").to route_to("ingredients#show", :id => "1")
+      expect(:get => "/api/ingredients/1").to_not route_to("ingredients#show", :id => "1")
     end
 
-
     it "routes to #create" do
-      expect(:post => "/ingredients").to route_to("ingredients#create")
+      expect(:post => "/api/ingredients").to_not route_to("ingredients#create")
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/ingredients/1").to route_to("ingredients#update", :id => "1")
+      expect(:put => "/api/ingredients/1").to_not route_to("ingredients#update", :id => "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/ingredients/1").to route_to("ingredients#update", :id => "1")
+      expect(:patch => "/api/ingredients/1").to_not route_to("ingredients#update", :id => "1")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/ingredients/1").to route_to("ingredients#destroy", :id => "1")
+      expect(:delete => "/api/ingredients/1").to_not route_to("ingredients#destroy", :id => "1")
     end
 
   end
