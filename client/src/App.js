342f1a556bce23998c7c1a8b367543abb89a8fb9
ceemion <fiyinsimeon@gmail.com>
2018-06-02 14:46:16 +0100

    Get drinks and drink from api

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Container, Header, Segment, Button, Icon, Dimmer, Loader, Divider } from 'semantic-ui-react';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {};
+
+    this.getDrinks = this.getDrinks.bind(this);
+    this.getDrink = this.getDrink.bind(this);
+  }
+
+  componentDidMount() {
+    this.getDrinks()
+  }
+
+  fetch(endpoint) {
+    return window.fetch(endpoint)
+      .then(response => response.json())
+      .catch(error => console.log(error));
+  }
+
+  getDrinks() {
+    this.fetch('/api/drinks')
+      .then(drinks => {
+        if (drinks.length) {
+          this.setState({drinks: drinks});
+          this.getDrink(drinks[0].id)
+        }
+        else {
+          this.setState({drinks: []})
+        }
+      })
+  }
+
+  getDrink(id) {
+    this.fetch(`/api/drinks/${id}`)
+      .then(drink => this.setState({drink: drink}))
+  }
+
   render() {
+    let {drink, drinks} = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
